b2bad92a8d0e9bd058226e7ee97a51b3cd0e8114
David Kezi <davidkezi6@gmail.com>
2016-05-19 21:25:56 +0300

    added progress bar js and edited container in index

--- js/app.js
+++ js/app.js
@@ -1,1 +1,37 @@
-$(document).foundation()
+var bar = new ProgressBar.Circle(container, {
+    color: '#aaa',
+    // This has to be the same size as the maximum width to
+    // prevent clipping
+    strokeWidth: 4,
+    trailWidth: 1,
+    easing: 'easeInOut',
+    duration: 1200,
+    text: {
+        autoStyleContainer: false
+    },
+    from: {
+        color: '#ff0',
+        width: 4
+    },
+    to: {
+        color: '#000',
+        width: 4
+    },
+    // Set default step function for all animate calls
+    step: function (state, circle) {
+        circle.path.setAttribute('stroke', state.color);
+        circle.path.setAttribute('stroke-width', state.width);
+
+        var value = Math.round(circle.value() * 100);
+        if (value === 0) {
+            circle.setText('');
+        } else {
+            circle.setText(value);
+        }
+
+    }
+});
+bar.text.style.fontFamily = '"Raleway", Helvetica, sans-serif';
+bar.text.style.fontSize = '2rem';
+
+bar.animate(0.75); // Number from 0.0 to 1.0
